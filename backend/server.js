4bf325b0c533ef8a44e459fd63506df6ef2c2298
Beckett Lewis <lewisbec@oregonstate.edu>
2022-11-10 13:41:29 -0800

    Add google datastore integration

--- backend/server.js
+++ backend/server.js
@@ -3,6 +3,32 @@ const app = express();
 
 const router = express.Router();
 const login = express.Router();
+const users = express.Router();
+
+// body parser for the req.body
+const bodyParser = require('body-parser');
+app.use(bodyParser.json());
+
+// datastore import and initialization
+const { Datastore } = require('@google-cloud/datastore');
+const datastore = new Datastore();
+const USERS = "Users";
+
+function fromDatastore(item) {
+    item.id = item[Datastore.KEY].id;
+    return item;
+}
+
+/* ------------- Begin Model Functions ------------- */
+
+function add_user(name, email) {
+    var key = datastore.key(USERS)
+    const new_user = { "name": name, "email": email }
+    return datastore.save({ "key": key, "data": new_user }).then((user) => { return user })
+}
+
+
+/* ------------- End Model Functions ------------- */
 
 
 
@@ -16,9 +42,17 @@ login.get('/', function (req, res) {
     res.send('Login route here with Auth0')
 })
 
+app.post('/users', function (req, res) {
+    console.log(req.body)
+    add_user(req.body.name, req.body.email).then((user) => {
+        res.status(201).json(user);
+    })
+})
+
 /* ------------- End Routes ------------- */
 
 
+app.use('/users', users);
 app.use('/questions', router);
 app.use('/login', login);
 
