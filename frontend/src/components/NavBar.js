a2c938d2dd9510bd07cdc5eb6e29a028ecae77ba
Symulation12 <Symulation12@gmail.com>
2023-03-09 15:15:02 -0800

    Re-enabled Auth0 Login

--- frontend/src/components/NavBar.js
+++ frontend/src/components/NavBar.js
@@ -105,6 +105,7 @@ const NavBar = () => {
                     id="qsLoginBtn"
                     color="primary"
                     className="btn-margin"
+                    onClick={() => loginWithRedirect({})}
                   >
                     Log in
                   </Button>
