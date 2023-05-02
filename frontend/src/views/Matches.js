20300db0a08eda083ff89bee3225fd598f026302
Beckett Lewis <lewisbec@oregonstate.edu>
2023-05-01 23:35:12 -0700

    Add matches basic formatting

--- frontend/src/views/Matches.js
+++ frontend/src/views/Matches.js
@@ -1,49 +1,50 @@
-import React, {useEffect, useState} from "react";
+import React, { useEffect, useState } from "react";
 import { Container, Row, Col } from "reactstrap";
 import Highlight from "../components/Highlight";
 import Loading from "../components/Loading";
 import { useAuth0, withAuthenticationRequired } from "@auth0/auth0-react";
+import User from '../components/User'
 
 export const MatchesComponent = () => {
   const { user } = useAuth0();
-  const [serverData, setServerData] = useState("");
+  const [serverData, setServerData] = useState([]);
 
   useEffect(() => {
     const reqOpts = {
       method: 'POST',
-      headers: {'Content-Type': 'application/json'},
-      body: JSON.stringify({user_id: user.email})
+      headers: { 'Content-Type': 'application/json' },
+      body: JSON.stringify({ user_id: user.email })
     };
     fetch('/matches', reqOpts)
-    .then(response => {
-      if(response.ok) {
-        return response.json();
-      }
-      throw response;
-    })
-    .then(data => {
-      setServerData(data);
-    }).catch(error => {
-      console.error("Error retrieving user data: " + error);
-    })
+      .then(response => {
+        if (response.ok) {
+          return response.json();
+        }
+        throw response;
+      })
+      .then(data => {
+
+        console.log(data)
+        setServerData(data);
+      }).catch(error => {
+        console.error("Error retrieving user data: " + error);
+      })
   }, []);
-        
-  
+
   return (
     <Container className="mb-5">
       <Row className="align-items-center profile-header mb-5 text-center text-md-left">
-        <Col md={2}>
-          Matches
-        </Col>
-        <Col md>
-          <h2>{user.name}</h2>
-          <p className="lead text-muted">{user.email}</p>
+        <Col>
+          <h1>Your Matches</h1>
         </Col>
       </Row>
-      <Row>
-        <Highlight>{JSON.stringify(serverData, null, 2)}</Highlight>
-      </Row>
-
+      {serverData.map((data) => (
+        <Row key={data.name_input} className="mb-3">
+          <Col>
+            <User data={data} />
+          </Col>
+        </Row>
+      ))}
     </Container>
   );
 };
