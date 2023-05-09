ee368cc24cee499c59332635eac8a64bbf3315ed
Beckett Lewis <lewisbec@oregonstate.edu>
2023-05-09 16:48:04 -0700

    remove unused routes

--- backend/server.js
+++ backend/server.js
@@ -125,7 +125,6 @@ async function get_matches(user_id) {
         includeScore: true,
     };
 
-    let fuse = new Fuse(options, fuseOptions);
     // for each question in questions list, update the search to only look at that key and with the certain weight desired, then look for the matches, update the scoring array for each one
     for (let question in user_questions) {
 
@@ -162,32 +161,10 @@ async function get_matches(user_id) {
 
 /* ------------- Begin USERS Routes --------------- */
 
-/* REQUEST FORMAT:
-    body: {
-        "name": 'STRING',
-        "email": 'STRING',
-        "user_id": Auth0 authentication number? 
-    }
-*/
-/*
-Disabling this given our users will be created for us by Auth0
-
-app.post("/users", async function (req, res) {
-    const user = await add_user(req.body.name, req.body.email, req.body.user_id);
-    res.status(201).json(user);
-});*/
-
 app.get("/users/:user_id", async function (req, res) {
     const user = await get_user(req.params.user_id);
     res.status(200).json(user);
 });
-
-/*
-Disabling because it currently isn't used
-app.get("/users", async function (req, res) {
-    const users = await get_users();
-    res.status(200).json(users);
-});*/
 
 /* REQUEST FORMAT:
     body: {
@@ -204,13 +181,6 @@ app.post("/users/", async function (req, res) {
     res.status(200).json(user);
 });
 
-/*
-app.delete("/users/:user_id", async function (req, res) {
-    var user_id = req.params.user_id;
-    await delete_user(user_id);
-    res.status(204).end();
-});
-*/
 
 /* 
 REQUEST FORMAT:
@@ -239,22 +209,6 @@ app.post("/matches", async function (req, res) {
 
 /* ------------- Begin QUESTION Routes --------------- */
 
-/* REQUEST FORMAT:
-    body: {
-        "type": STRING (mentor/mentee/both),
-        "question": STRING,
-        "options": [STRING, STRING, STRING, (ctn...)]
-    }
-*/
-/*
-app.post("/questions", async function (req, res) {
-    const type = req.body.type;
-    const question = req.body.question;
-    const options = req.body.options;
-    const response = await add_questions(type, question, options)
-    res.status(201).json(response);
-});
-*/
 
 app.get("/questions/:type", async function (req, res) {
     const type = req.params.type;
